391e24d7bbc8305fb5e426f3431a563e876ba49a
Ruslan Belyy <ruslanbelyy@gmail.com>
2024-04-12 15:02:56 -0400

    fixed email

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,16 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 // Middleware to parse JSON bodies
 app.use(express.json());
 
+app.get("/", async (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
 app.post("/", async (req: Request, res: Response) => {
   try {
-    const htmlToSend = processTemplate(2, req.body);
-    const emailResult = sendEmail("antonio@pixel", "Test", htmlToSend);
+    const emailData = req.body.email;
+    const templateData = req.body.data;
+    const htmlToSend = processTemplate(emailData.templateId, templateData);
+    const emailResult = sendEmail(emailData.to, emailData.subject, htmlToSend);
     return emailResult;
   } catch (error) {
     res.status(500).json({ error });
